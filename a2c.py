7170c749f67f89021bd5076610b1a4486840cbe2
ethanluoyc <ethanluoyc@gmail.com>
2018-02-09 19:56:10 +0000

    update a2c

--- a2c.py
+++ a2c.py
@@ -1,15 +1,18 @@
 """Advantageous Actor Critic (A2C)"""
+import torch
 from torch import nn
-import torch.functional as F
+import torch.nn.functional as F
+from torch import autograd
+import numpy as np
 
 
 # TODO Gaussian policy (square error)
 # TODO Discrete policy (cross entropy)
 
 
-class A2C(nn.Module):
+class PG(nn.Module):
     def __init__(self):
-        super(A2C, self).__init__()
+        super(PG, self).__init__()
         self.fc1 = nn.Linear(4, 200)
         self.bn1 = nn.BatchNorm1d(200)
         self.fc2 = nn.Linear(200, 200)
@@ -37,3 +40,92 @@ class A2C(nn.Module):
             param.grad.data.clamp_(-1, 1)
 
         optimizer.step()
+
+
+class A2C(nn.Module):
+    def __init__(self):
+        super(A2C, self).__init__()
+        self.v_fc1 = nn.Linear(4, 200)
+        self.v_bn1 = nn.BatchNorm1d(200)
+        self.v_fc2 = nn.Linear(200, 200)
+        self.v_bn2 = nn.BatchNorm1d(200)
+        self.v_head = nn.Linear(200, 1)
+
+        self.p_fc1 = nn.Linear(4, 200)
+        self.p_bn1 = nn.BatchNorm1d(200)
+        self.p_fc2 = nn.Linear(200, 200)
+        self.p_bn2 = nn.BatchNorm1d(200)
+        self.p_head = nn.Linear(200, 2)
+
+    def _value(self, s):
+        x = F.leaky_relu((self.v_fc1(s)))
+        x = F.leaky_relu((self.v_fc2(x)))
+        return self.v_head(x)
+
+    def _policy(self, s):
+        x = F.leaky_relu((self.p_fc1(s)))
+        x = F.leaky_relu((self.p_fc2(x)))
+        return self.p_head(x)
+
+    def _var(self, x):
+        return torch.autograd.Variable(torch.from_numpy(x)).float().unsqueeze(0)
+
+    def train_me(self, env, gamma=.99):
+        optimizer = torch.optim.Adam(self.parameters())
+
+        for _ in range(1000):
+            s = env.reset()
+            s = self._var(s)
+            while True:
+                predicted_actions = self._policy(s)
+
+                actions = np.atleast_1d(env.action_space.sample())
+                actions = self._var(actions).long()
+
+                snext, r, done, _ = env.step(predicted_actions.max(1)[1].numpy()[0])
+
+                snext = self._var(snext)
+                r = self._var(np.atleast_1d(r))
+
+                Vsnext = self._value(snext)
+                Vs = self._value(s)
+
+                target = r + gamma * Vsnext
+                target = target.detach()
+
+                # print(Vs); print(target)
+
+                loss = F.smooth_l1_loss(Vs, target)
+
+                # optimizer.zero_grad()
+                # loss.backward()
+                # for param in self.parameters():
+                #     if param.grad is not None:
+                #         param.grad.data.clamp_(-1, 1)
+                # optimizer.step()
+
+                A = target - Vs  # Advantage
+                # print(predicted_actions)
+                # print(actions)
+
+                nll = nn.CrossEntropyLoss(reduce=False)(predicted_actions, actions[:, 0])
+                l = nll.mul(A).mean()
+
+                optimizer.zero_grad()
+                l.backward()
+                for param in self.parameters():
+                    if param.grad is not None:
+                        param.grad.data.clamp_(-1, 1)
+                optimizer.step()
+
+                s = snext
+                if done:
+                    break
+
+
+if __name__ == "__main__":
+    import gym
+
+    env = gym.make('CartPole-v0')
+    learner = A2C()
+    learner.train_me(env)
